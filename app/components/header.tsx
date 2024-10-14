75add775642c97951a0b8c4e7eb28ec56f362116
mashachitchyan <margarita.chitchyan@gmail.com>
2024-10-14 19:39:04 +0000

    Three pages with Next.js

---
+++ app/components/header.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Header() {
+    return <>
+
+        <div className="container py-4">
+            <div className="row">
+
+                <div className="col-12 mb-4">
+                    <h1>Elevate Your Coffee Experience</h1>
+                    <p className="small m-0">
+                        Dive into the world of artisanal coffee roasting
+                    </p>
+                </div>
+            </div>
+        </div>
+
+    </>
+}
